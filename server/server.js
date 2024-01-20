44a591f7b40711f6e620289a1571b5231c75c364
Phifey <SinanSaritas@outlook.com>
2024-01-20 22:05:04 +0100

    setting up api

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
+const workoutRoutes = require("./routes/workouts")
 
 // express app
 const app = express();
@@ -12,11 +13,9 @@ app.use((req, res, next) => {
 });
 
 // routes
-app.get('/', (req, res) => {
-    res.json({message: "Welcome to the app"})
-})
+app.use("/api/workouts", workoutRoutes);
 
 // listen for requests
 app.listen(process.env.PORT, () => {
-    console.log('listening on port', process.env.PORT);
+    console.log("listening on port", process.env.PORT);
 });
