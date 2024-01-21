d96aaded271f8b83c4a533a3ee39c585d0e846e1
Phifey <SinanSaritas@outlook.com>
2024-01-21 22:32:03 +0100

    created first schema and model for posting to db

--- server/routes/workouts.js
+++ server/routes/workouts.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const Workout = require("../models/workoutModel");
 
 const router = express.Router();
 
@@ -13,8 +14,15 @@ router.get('/:id', (req, res) => {
 });
 
 // POST a new workout
-router.post('/', (req, res) => {
-    res.json({message: "POST a new workout"})
+router.post('/', async (req, res) => {
+    const {title, load, reps} = req.body;
+    
+    try{
+        const workout = await Workout.create({title, load, reps});
+        res.status(200).json(workout);
+    } catch (error) {
+        res.status(400).json({error: error.message});
+    }
 });
 
 // DELETE a workout
@@ -24,7 +32,7 @@ router.delete('/:id', (req, res) => {
 
 // UPDATE a workout
 router.patch('/:id', (req, res) => {
-    res.json({message: "update a workout"})
+    res.json({message: "UPDATE a workout"})
 });
 
 module.exports = router;
