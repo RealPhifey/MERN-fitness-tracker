0193a66a3d400b6b3c194fdcf68d9cef7ee988a5
Phifey <SinanSaritas@outlook.com>
2024-01-22 03:36:25 +0100

    finished setting up controller

--- server/routes/workouts.js
+++ server/routes/workouts.js
@@ -3,6 +3,8 @@ const {
     createWorkout,
     getWorkouts,
     getWorkout,
+    deleteWorkout,
+    updateWorkout
 } = require("../controllers/workoutController");
 
 const router = express.Router();
@@ -17,13 +19,9 @@ router.get('/:id', getWorkout);
 router.post('/', createWorkout);
 
 // DELETE a workout
-router.delete('/:id', (req, res) => {
-    res.json({message: "DELETE a workout"})
-});
+router.delete('/:id', deleteWorkout);
 
 // UPDATE a workout
-router.patch('/:id', (req, res) => {
-    res.json({message: "UPDATE a workout"})
-});
+router.patch('/:id', updateWorkout);
 
 module.exports = router;
