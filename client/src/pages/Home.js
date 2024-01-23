a1481c6c7ffe3343c256f6b23d307b813ed7bfa7
sinansaritas <29292230+RealPhifey@users.noreply.github.com>
2024-01-24 00:09:11 +0100

    Enable frontend data display with backend integration

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,31 @@
+import { useEffect, useState }from "react";
+
+// components
+import WorkoutDetails from "../components/WorkoutDetails";
+
 const Home = () => {
+    const [workouts, setWorkouts] = useState(null);
+
+    useEffect(() => {
+        const fetchWorkouts = async () => {
+            const respone = await fetch("/api/workouts");
+            const json = await respone.json();
+
+            if (respone.ok) {
+                setWorkouts(json);
+            }
+        };
+
+        fetchWorkouts();
+    }, [])
+
     return (
         <div className="home">
-            <h2>Home</h2>
+            <div className="workouts">
+                {workouts && workouts.map((workout) => (
+                    <WorkoutDetails key={workout._id} workout={workout} />
+                ))}
+            </div>
         </div>
     )
 };
